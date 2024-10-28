5fd70443fd7584bbe77108d0f0368aca381be393
MrDAH34 <anhhao22082004@gmail.com>
2024-10-28 23:30:32 +0700

    fix product.html, display order info

--- src/main/java/com/example/ecommerce/controller/AdminController.java
+++ src/main/java/com/example/ecommerce/controller/AdminController.java
@@ -2,17 +2,17 @@ package com.example.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
+// @RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("/product")
+    @GetMapping("/products")
     public String productManage() {
         return "product";
     }
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     public String userManage() {
         return "user";
     }
--- src/main/java/com/example/ecommerce/dto/OrderDTO.java
+++ src/main/java/com/example/ecommerce/dto/OrderDTO.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.dto;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import lombok.Data;
 
@@ -12,7 +13,17 @@ public class OrderDTO {
     private String phone;
     private String email;
     private BigDecimal shippingPrice;
-    private Date createdAt;
-    private Date shippingDate;
+    private String createdAt;
+    private String shippingDate;
     private byte status;
+
+    public void setCreatedAt(Date createdAt) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        this.createdAt = formatter.format(createdAt);
+    }
+
+    public void setShippingDate(Date shippingDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        this.shippingDate = formatter.format(shippingDate);
+    }
 }
